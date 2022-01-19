4e2514727b11f6ce0c23f05b35928db9f5a1154b
sycha <sychang0422@gmail.com>
2022-01-19 16:50:45 +0900

    빈 생명주기 콜백

---
+++ src/test/java/hello/core/lifeCycle/NetworkClient.java
@@ -0,0 +1,41 @@
+package hello.core.lifeCycle;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class NetworkClient {
+
+    private String url;
+
+    public NetworkClient() {
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    // 서비스 시작 시 호출
+    public void connect() {
+        System.out.println("connect: "+ url);
+    }
+
+    public void call(String message) {
+        System.out.println("call: "+url + "message: "+ message);
+    }
+
+    //서비스 종료 시 호출
+    public void disconnect() {
+        System.out.println("close: " + url);
+    }
+
+    @PostConstruct
+    private void init() {
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @PreDestroy
+    private void close() {
+        disconnect();
+    }
+}
